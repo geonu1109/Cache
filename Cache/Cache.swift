9793b98a4ea765fbe30bebb3a7c096dc5279a641
Geonu Jeon <geonu1109@daou.co.kr>
2019-12-15 17:14:25 +0900

    implement cache

--- Cache/Cache.swift
+++ Cache/Cache.swift
@@ -8,9 +8,7 @@
 
 import Foundation
 
-class Cache: ObservableObject {
-    private var sets: [CacheSet]
-    
+class CacheConfiguration {
     let numberOfSets: Int
     let numberOfBlocks: Int
     let numberOfWords: Int
@@ -19,48 +17,118 @@ class Cache: ObservableObject {
         self.numberOfSets = numberOfSets
         self.numberOfBlocks = numberOfBlocks
         self.numberOfWords = numberOfWords
-        
-        self.sets = (0 ..< numberOfSets).map { _ in CacheSet(numberOfBlocks: numberOfBlocks, numberOfWords: numberOfWords) }
-    }
-    
-    func findAddress(_ address: Int) -> Bool {
-        let index: Int = address / (self.numberOfSets * self.numberOfWords)
-        return self.sets[index].findAddress(address)
     }
 }
 
-fileprivate class CacheSet {
+class Cache: CustomDebugStringConvertible {
+    let configuration: CacheConfiguration
+    let sets: [CacheSet]
+    
+    var debugDescription: String {
+        return sets.enumerated().map { (index, set) in
+            return "set\(index):\n\(set.debugDescription)"
+        }.joined(separator: "\n")
+    }
+    
+    init(configuration: CacheConfiguration) {
+        self.configuration = configuration
+        self.sets = (0 ..< configuration.numberOfSets).map { _ in CacheSet(configuration: configuration) }
+    }
+    
+    func findAddress(_ address: Int) -> Bool {
+        let index: Int = address / (self.configuration.numberOfWords * 4) % self.configuration.numberOfSets
+        let result = self.sets[index].findAddress(address)
+        self.sets.forEach { $0.increaseAge() }
+        return result
+    }
+}
+
+class CacheSet: CustomDebugStringConvertible {
+    let configuration: CacheConfiguration
     let blocks: [CacheBlock]
     
-    let numberOfBlocks: Int
-    let numberOfWords: Int
+    var debugDescription: String {
+        return blocks.enumerated().map { (index, block) in
+            return "block\(index): \(block.debugDescription)"
+        }.joined(separator: "\n")
+    }
     
-    init(numberOfBlocks: Int, numberOfWords: Int) {
-        self.numberOfBlocks = numberOfBlocks
-        self.numberOfWords = numberOfWords
-        
-        self.blocks = (0 ..< numberOfBlocks).map { _ in CacheBlock(numberOfWords: numberOfWords) }
+    init(configuration: CacheConfiguration) {
+        self.configuration = configuration
+        self.blocks = (0 ..< configuration.numberOfBlocks).map { _ in CacheBlock(configuration: configuration) }
     }
     
     func findAddress(_ address: Int) -> Bool {
-        for block in blocks {
-            guard let startAddress = block.startAddress else {
-                continue
-            }
-            if startAddress == (address - address % numberOfWords) {
-                block.age = 0
-                return true
-            }
+        let result: Bool = blocks.map { (block) -> Bool in
+            return block.hasAddress(address)
+        }.reduce(false) { $0 || $1 }
+        
+        if result {
+            return true
         }
+        else {
+            let index = blocks.enumerated().reduce(nil) { (result: (Int, CacheBlock)?, element) in
+                let (index, cache) = element
+                
+                guard let result = result else {
+                    return element
+                }
+                if result.1.startAddress == nil {
+                    return result
+                }
+                else if cache.startAddress == nil {
+                    return element
+                }
+                else {
+                    return result.1.age < cache.age ? element : result
+                }
+            }?.0 ?? 0
+            blocks[index].setAddress(address)
+            return false
+        }
+    }
+    
+    func increaseAge() {
+        self.blocks.forEach { $0.increaseAge() }
     }
 }
 
-fileprivate class CacheBlock {
+class CacheBlock: CustomDebugStringConvertible {
+    let configuration: CacheConfiguration
     var startAddress: Int? = nil
+    var endAddress: Int? {
+        return startAddress.map { $0 + self.configuration.numberOfWords * 4 - 1 }
+    }
     var age: Int = 0
-    let numberOfWords: Int
     
-    init(numberOfWords: Int) {
-        self.numberOfWords = numberOfWords
+    var debugDescription: String {
+        return "startAddress: \(startAddress.flatMap { String($0, radix: 16) } ?? "nil"), age: \(age)"
+    }
+    
+    init(configuration: CacheConfiguration) {
+        self.configuration = configuration
+    }
+    
+    func hasAddress(_ address: Int) -> Bool {
+        guard let startAddress = self.startAddress, let endAddress = self.endAddress else {
+            return false
+        }
+        let result: Bool = address >= startAddress && address <= endAddress
+        if result {
+            self.age = 0
+        }
+        return result
+    }
+    
+    func setAddress(_ address: Int) {
+        self.startAddress = address - address % (self.configuration.numberOfWords * 4)
+        self.age = 0
+    }
+    
+    func increaseAge() {
+        guard startAddress != nil else {
+            return
+        }
+        self.age += 1
     }
 }
--- Cache/ContentView.swift
+++ Cache/ContentView.swift
@@ -9,15 +9,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var viewModel: ViewModel = .init()
+    let configuration: CacheConfiguration
+    let cache: Cache
+    
+    @State var text: String = ""
     
     var body: some View {
-        VStack {
-            ConfigurationView(numberOfSets: $viewModel.numberOfSets, numberOfBlocks: $viewModel.numberOfBlocks, numberOfWords: $viewModel.numberOfWords).padding()
-            List {
-                /*@START_MENU_TOKEN@*/ /*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+        HStack {
+            TextField("주소 입력", text: self.$text).textFieldStyle(RoundedBorderTextFieldStyle())
+            Button(action: self.buttonTapped) {
+                Text("입력")
             }
+        }.padding()
+    }
+    
+    init() {
+        let configuration: CacheConfiguration = .init(numberOfSets: 2, numberOfBlocks: 2, numberOfWords: 1)
+        let cache: Cache = .init(configuration: configuration)
+        
+        self.configuration = configuration
+        self.cache = cache
+    }
+    
+    func startConsole() {
+        while true {
+            guard let address: Int = readLine().flatMap(Int.init) else {
+                print("fail to convert input text to number")
+                continue
+            }
+            print("address: \(String(address, radix: 16))")
         }
+        
+    }
+    
+    func buttonTapped() {
+        guard let address: Int = Int(self.text, radix: 16) else {
+            print("변환 실패")
+            return
+        }
+        print("address: \(String(address, radix: 16))")
+        if self.cache.findAddress(address) {
+            print("hit")
+        }
+        else {
+            print("miss")
+        }
+        self.text = ""
+        print(self.cache.debugDescription)
     }
 }
 
